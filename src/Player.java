dfbec94329d11de35fb94208706052efe1f1622c
student <jasper_hsu@student.wayland.k12.ma.us>
2018-05-29 19:58:08 -0400

    Player

--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,58 @@
-public class Player extends Sprite {
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Player extends Sprite implements KeyListener {
+    private boolean moveLeft, moveRight, moveUp, moveDown;
+
     public Player(int x, int y){
         super(x, y);
+    }
+
+    public void update(Graphics2D g2){
+        if(moveLeft){
+            move(0);
+        } else if(moveRight){
+            move(1);
+        } else if(moveUp){
+            move(2);
+        } else if(moveDown){
+            move(3);
+        }
+        draw(g2);
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int keyCode = e.getKeyCode();
+
+        if(keyCode == KeyEvent.VK_A){
+            moveLeft = true;
+        } else if(keyCode == KeyEvent.VK_D){
+            moveRight = true;
+        } else if(keyCode == KeyEvent.VK_W){
+            moveUp = true;
+        } else if(keyCode == KeyEvent.VK_S){
+            moveDown = true;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int keyCode = e.getKeyCode();
+
+        if(keyCode == KeyEvent.VK_A){
+            moveLeft = false;
+        } else if(keyCode == KeyEvent.VK_D){
+            moveRight = false;
+        } else if(keyCode == KeyEvent.VK_W){
+            moveUp = false;
+        } else if(keyCode == KeyEvent.VK_S){
+            moveDown = false;
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
     }
 }
