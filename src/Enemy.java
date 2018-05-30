466dab7dba31159b0137e3250ded175db583bf73
student <jasper_hsu@student.wayland.k12.ma.us>
2018-05-30 17:40:07 -0400

    Enemy, added random movement

---
+++ src/Enemy.java
@@ -0,0 +1,51 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+public class Enemy extends Sprite {
+    private boolean hasRandomLoc;
+
+    private int randomX, randomY, randomRange;
+
+    public Enemy(int x, int y){
+        super(x, y);
+        hasRandomLoc = false;
+        randomRange = 200;
+
+        Timer randomlyMove = new Timer(1000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(new Random().nextInt(100) < 50 && hasRandomLoc == false) {
+                    randomX = (new Random().nextInt(randomRange*2) - randomRange + getX())/getSpeed()*getSpeed();
+                    randomY = (new Random().nextInt(randomRange*2) - randomRange + getY())/getSpeed()*getSpeed();
+                    hasRandomLoc = true;
+                }
+            }
+        });
+        randomlyMove.start();
+    }
+
+    public void update(Graphics2D g2){
+        if(hasRandomLoc){
+            if(getX() != randomX){
+                if(randomX < getX()){
+                    move(0);
+                } else {
+                    move(1);
+                }
+            } else if(getY() != randomY){
+                if(randomY < getY()){
+                    move(2);
+                } else {
+                    move(3);
+                }
+            } else {
+                hasRandomLoc = false;
+            }
+        }
+
+        draw(g2);
+    }
+}
