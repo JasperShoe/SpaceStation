6b78733edd4c1d4db8aef9eacab97361a49998f5
student <jasper_hsu@student.wayland.k12.ma.us>
2018-05-30 12:16:49 -0400

    Created Movement

--- src/GraphicsPanel.java
+++ src/GraphicsPanel.java
@@ -4,47 +4,30 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class GraphicsPanel extends JPanel {
-
-    //=====instance fields=====
-
     private Player player;
 
-    //=====constructor=====
-
     public GraphicsPanel(){
-
         setSize(WIDTH, HEIGHT);
         setLayout(null);
         setFocusable(true);
 
-        player = new Player(100, 100);
+        player = new Player(400-16, 400-16);
         player.setSpeed(4);
         player.setImg(Images.player_front);
         addKeyListener(player);
 
         Timer update = new Timer(1000 / 60, new ActionListener() {
             @Override
-
             public void actionPerformed(ActionEvent e) {
-
                 repaint();
-
-            }//end actionPerformed
-
-        });//end timer
-
+            }
+        });
         update.start();
-
-    }//end GraphicsPanel
-
-    //=====methods=====
+    }
 
     public void paintComponent(Graphics g){
-
         Graphics2D g2 = (Graphics2D) g;
-
+        g2.translate(player.getTransX(), player.getTransY());
         player.update(g2);
-
-    }//end paintComponent
-
-}//end class
+    }
+}
