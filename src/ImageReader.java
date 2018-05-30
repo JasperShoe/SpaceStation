4b33da49f04eaa879a062dbda1189eeedb708231
Toasterdoodle <michael_chen@student.wayland.k12.ma.us>
2018-05-30 12:09:21 -0400

    comments

--- src/ImageReader.java
+++ src/ImageReader.java
@@ -4,15 +4,25 @@ import java.io.File;
 import java.io.IOException;
 
 public class ImageReader {
+
     public BufferedImage readBufferedImage(String imageName){
+
         BufferedImage img = null;
+
         try {
+
             img = ImageIO.read(new File("./res/" + imageName + ".png"));
+
         } catch (IOException e) {
+
             // TODO Auto-generated catch block
+
             e.printStackTrace();
-        }
+
+        }//end try catch
 
         return img;
-    }
-}
+
+    }//end readBufferedImage
+
+}//end class
--- src/Sprite.java
+++ src/Sprite.java
@@ -2,38 +2,59 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class Sprite {
+
+    //=====instance fields=====
+
     private int x, y, speed;
 
     private boolean isAlive;
 
     private BufferedImage img;
 
+    //=====constructor=====
+
     public Sprite(int x, int y){
+
         this.x = x;
         this.y = y;
         isAlive = true;
-    }
+
+    }//end Sprite
+
+    //=====methods=====
 
     public void draw(Graphics2D g2){
+
         g2.drawImage(img, null, x, y);
-    }
+
+    }//end draw
 
     public void move(int dir){
+
         int vX = 0, vY = 0;
 
         if(dir == 0){ //Left
+
             vX = -speed;
+
         } else if (dir == 1){ //Right
+
             vX = speed;
+
         } else if(dir == 2){ //Up
+
             vY = -speed;
+
         }  else if(dir == 3){ //Down
+
             vY = speed;
-        }
+
+        }//end if chain
 
         x += vX;
         y += vY;
-    }
+
+    }//end move
 
     //Setters
     public void setX(int x){
@@ -68,4 +89,5 @@ public class Sprite {
     public boolean isAlive(){
         return isAlive;
     }
-}
+
+}//end class
