500cf919dd1682e2519b4431dcc56235c8da78f3
student <jasper_hsu@student.wayland.k12.ma.us>
2018-06-18 12:22:59 -0400

    Fixed

---
+++ src/Client/Map.java
@@ -0,0 +1,14 @@
+package Client;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Map extends JPanel {
+    public Map(){
+        setSize(100, 100);
+    }
+
+    public void paintComponent(Graphics g){
+        Graphics2D g2 = (Graphics2D) g;
+    }
+}
